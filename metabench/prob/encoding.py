9a2c8da2bf6ddcefecca0565f47ea3de5e33dbf7
ComeBertrand <bertrand.cosme@gmail.com>
2017-03-05 17:12:20 +0100

    Change on the encodings, now they are numpy ndarrays and they hold their distance function

--- metabench/prob/encoding.py
+++ metabench/prob/encoding.py
@@ -1,86 +1,72 @@
 import random
 import math
 
+import numpy as np
 
-def hamming_distance(s1, s2):
-    if len(s1) != len(s2):
-        raise ValueError("Undefined for sequence of unequal length")
-    return sum([el1 != el2 for el1, el2 in zip(s1, s2)])
+import metabench.misc.distances as dist
 
 
-def manhattan_distance(s1, s2):
-    if len(s1) != len(s2):
-        raise ValueError("Undefined for sequence of unequal length")
-    return sum([abs(el1 - el2) for el1, el2 in zip(s1, s2)])
-
-
-def euclidian_distance(s1, s2):
-    if len(s1) != len(s2):
-        raise ValueError("Undefined for sequence of unequal length")
-    return math.sqrt(sum([abs(el1 - el2) ** 2 for el1, el2 in zip(s1, s2)]))
-
-
-class Encoding(object):
+class Encoding(np.ndarray):
     pass
 
 
 class BinaryEncoding(Encoding):
-    def __init__(self, size):
+    def __init__(self, size, distance=dist.hamming_distance):
         super().__init__()
         self.size = size
+        self.distance = distance
 
     def generate_random_solution(self):
-        sol = []
-        for i in range(self.size):
-            sol.append(random.randint(0, 1))
-        return sol
+        return np.random.randint(2, size=self.size)
 
     def space_size(self):
         return 2 ** self.size
 
     def distance(self, solution_1, solution_2):
-        return hamming_distance(solution_1, solution_2)
+        return self.distance(solution_1, solution_2)
 
 
 class DiscreteEncoding(Encoding):
-    def __init__(self, boundaries):
+    def __init__(self, boundaries, distance=dist.manhattan_distance):
         super().__init__()
         self.boundaries = boundaries
+        self.distance = distance
 
     def generate_random_solution(self):
         sol = []
         for i in range(len(self.boundaries)):
-            sol.append(random.randint(self.boundaries[i][0],
-                                      self.boundaries[i][1]))
+            sol.append(np.random.randint(self.boundaries[i][0],
+                                         self.boundaries[i][1]))
         return sol
 
     def space_size(self):
         size = 1.
         for i in range(len(self.boundaries)):
-            size *= len(range(self.boundaries[i][0], self.boundaries[i][1]))
+            size *= (self.boundaries[i][1] - self.boundaries[i][0]) + 1
         return size
 
     def distance(self, solution_1, solution_2):
-        return manhattan_distance(solution_1, solution_2)
+        return self.distance(solution_1, solution_2)
 
 
 class RealVector(Encoding):
-    def __init__(self, boundaries):
+    def __init__(self, boundaries, distance=dist.euclidian_distance):
         super().__init__()
         self.boundaries = boundaries
+        self.distance = distance
 
     def generate_random_solution(self):
         sol = []
         for i in range(len(self.boundaries)):
-            sol.append(random.uniform(self.boundaries[i][0],
-                                      self.boundaries[i][1]))
+            sol.append(np.random.uniform(self.boundaries[i][0],
+                                         self.boundaries[i][1]))
         return sol
 
     def space_size(self):
         return None
 
     def distance(self, solution_1, solution_2):
-        return euclidian_distance(solution_1, solution_2)
+        return self.distance(solution_1, solution_2)
 
 
 class PermutationEncoding(Encoding):
@@ -107,17 +93,17 @@ class MixedEncoding(Encoding):
         sol = []
         for i in range(len(self.boundaries)):
             if self.types[i] is bool:
-                sol.append(random.randint(0, 1))
+                sol.append(np.random.randint(2))
             elif self.types[i] is int:
-                sol.append(random.randint(self.boundaries[i][0],
-                                          self.boundaries[i][1]))
+                sol.append(np.random.randint(self.boundaries[i][0],
+                                             self.boundaries[i][1]))
             elif self.types[i] is float:
-                sol.append(random.uniform(self.boundaries[i][0],
-                                          self.boundaries[i][1]))
+                sol.append(np.random.uniform(self.boundaries[i][0],
+                                             self.boundaries[i][1]))
             elif self.types[i] is list:
-                sol.append(random.choice(self.boundaries[i]))
+                sol.append(np.random.choice(self.boundaries[i]))
             elif self.types[i] is str:
-                sol.append(random.choice(self.boundaries[i]))
+                sol.append(np.random.choice(self.boundaries[i]))
             else:
                 raise TypeError("Unknown type of variable at index {:d} : \
                                 {}".format(i, self.types[i]))
