a455b1dc05cea55b1011597588dcee56cad1d4fb
ComeBertrand <bertrand.cosme@gmail.com>
2017-03-09 19:35:37 +0100

    Reinternalize the distance computation in the encodings rather than having a separate module for it. Add some doc.

--- metabench/prob/encoding.py
+++ metabench/prob/encoding.py
@@ -7,11 +7,10 @@ Description: Solution encoding and generation in a defined space.
 """
 
 import math
-from functools import partial
+from functools import reduce
 
 import numpy as np
 
-from metabench.misc.distances import sol_distance
 from metabench.prob.solution import Solution
 
 
@@ -102,18 +101,18 @@ class Encoding(object):
     Args:
         boundaries (Boundaries): Boundaries on the solution space. Default is
             None.
-        distance (func): Normalized function to compute the distance between
-            two solutions. Default is None.
+        ord (int): Order of the norm used for distance computation (if a
+            distance is defined on the solution space). Default is None.
 
     Attributes:
         boundaries (Boundaries): Boundaries on the solution space.
-        distance (func): Normalized function to compute the distance between
-            two solutions.
+        ord (int): Order of the norm used for distance computation (if a
+            distance is defined on the solution space).
 
     """
-    def __init__(self, boundaries=None, distance=None):
+    def __init__(self, boundaries=None, ord=None):
         self.boundaries = boundaries
-        self.distance = distance
+        self.ord = ord
 
     def generate_random_solution(self):
         """Generate a random candidate solution.
@@ -133,11 +132,13 @@ class Encoding(object):
         """
         raise NotImplementedError('Abstract Class')
 
-
     def distance(self, solution_1, solution_2):
         """Get the distance between two candidate solutions arrays.
 
         These solution arrays must be of the proper encoding.
+
+        The difference between two solution arrays will be normalized if
+        boundaries are defined on the solution space.
 
         Args:
             solution_1 (Solution): First array.
@@ -147,36 +148,40 @@ class Encoding(object):
             float: the distance between the two solutions arrays.
 
         """
-        if self.distance is None:
+        if self.ord is None:
             raise NotImplementedError('No distance is given to this encoding')
+
         if solution_1.encoding != self or solution_2.encoding != self:
             raise ValueError('{0.__class__.__name__} can only compute distance'
                              ' between solutions that are encoded with '
                              'itself.'.format(self))
-        return self.distance(solution_1, solution_2)
+
+        diff = solution_1 - solution_2
+        if self.boundaries:
+            diff = self.boundaries.normalize(diff)
+
+        return np.linalg.norm(diff, ord=self.ord)
 
 
 class BinaryEncoding(Encoding):
     """Encode a solution as an array of binary values.
 
+    The norm used to compute distances in the solution space is the hamming
+    distance (norm of order 0).
+
     Args:
         size (int): Size of the solution array. Must be positive.
-        distance (func): Distance function used between solutions. Default is
-            None (meaning than the hamming distance will be used (norm 0)).
 
     Attributes:
         size (int): Size of the solution array.
 
     """
-    def __init__(self, size, distance=None):
+    def __init__(self, size):
         if size <= 0:
             raise ValueError('Cannot encode solution on an array of negative'
                              ' or empty size')
 
-        if distance is None:
-            distance = partial(sol_distance, ord=0)
-
-        super().__init__(distance=distance)
+        super().__init__(ord=0)
 
         self.size = size
 
@@ -198,67 +203,103 @@ class BinaryEncoding(Encoding):
 
 
 class DiscreteEncoding(Encoding):
-    def __init__(self, boundaries, distance=None):
-        if distance is None:
-            distance = partial(sol_distance,
-                               normalize=boundaries.normalize,
-                               ord=1)
-        super().__init__(boundaries, distance)
-        self.size = len(boundaries)
+    """Encode a solution as an array of integer values.
+
+    The norm used to compute distances in the solution space is the manhattan
+    distance (norm of order 1).
+
+    Args:
+        boundaries (Boundaries): Boundaries on the solution space.
+
+    """
+    def __init__(self, boundaries):
+        super().__init__(boundaries, ord=1)
 
     def generate_random_solution(self):
-        values = []
-        for i in range(self.size):
-            values.append(np.random.random_integers
-                          (self.boundaries[0][i], self.boundaries[1][i]))
-        solution = Solution(np.array(values, np.int), self)
+        """Generate a random solution.
+
+        Randomly create an integer array with uniform distribution on the
+        solution space of each index.
+
+        Returns:
+            Solution: A random solution array.
+
+        """
+        values = np.zeros(self.boundaries.shape[1], np.int)
+        for i in range(self.boundaries.shape[1]):
+            values[i] = np.random.random_integers(self.boundaries[0][i],
+                                                  self.boundaries[1][i])
+        solution = Solution(values, self)
         return solution
 
     def space_size(self):
-        space_size = 1.
-        for i in range(self.size):
-            space_size *= (self.boundaries[1][i] - self.boundaries[0][i] + 1)
+        space_size = reduce(lambda x, y: x*y, self.boundaries[2] + 1)
         return space_size
 
 
 class RealVector(Encoding):
-    def __init__(self, boundaries, distance=None):
-        if distance is None:
-            distance = partial(sol_distance,
-                               normalize=boundaries.normalize,
-                               ord=2)
-        super().__init__(boundaries, distance)
+    """Encode a solution as an array of float values.
+
+    The norm used to compute distances in the solution space is the euclidian
+    distance (norm of order 2).
+
+    Args:
+        boundaries (Boundaries): Boundaries on the solution space.
+
+    """
+    def __init__(self, boundaries):
+        super().__init__(boundaries, ord=2)
 
     def generate_random_solution(self):
-        values = []
-        for i in range(len(self.boundaries)):
-            values.append(np.random.uniform(self.boundaries[0][i],
-                                            self.boundaries[1][i]))
-        solution = Solution(np.array(values, np.float), self)
-        return solution
+        """Generate a random solution.
 
-    def space_size(self):
-        return None
+        Randomly create an integer array with uniform distribution on the
+        solution space of each index.
+
+        Returns:
+            Solution: A random solution array.
+
+        """
+        values = np.zeros(self.boundaries.shape[1], np.float)
+        for i in range(self.boundaries.shape[1]):
+            values[i] = np.random.uniform(self.boundaries[0][i],
+                                          self.boundaries[1][i])
+        solution = Solution(values, self)
+        return solution
 
 
 class PermutationEncoding(Encoding):
+    """Encode a solution as permutation of a list of items.
+
+    No distance is defined on the solution space of the permutations.
+
+    Args:
+        items (list): Items that will be permuted.
+
+    Attributes:
+        items (np.array): Items that will be permuted.
+
+    """
     def __init__(self, items):
         super().__init__()
-        self.items = items
-        self.size = len(items)
+        self.items = np.array(items)
 
     def generate_random_solution(self):
-        values = np.array(self.items)
+        """Generate a random solution.
+
+        Randomly create a permutation of the items.
+
+        Returns:
+            Solution: A random solution array.
+
+        """
+        values = np.array(self.items, copy=True)
         np.random.shuffle(values)
         solution = Solution(values, self)
         return solution
 
     def space_size(self):
         return math.factorial(self.size)
-
-    def distance(self, solution_1, solution_2):
-        raise NotImplementedError('Distance functions are not implemented for '
-                                  'permutation based solution.')
 
 
 class MixedEncoding(Encoding):
